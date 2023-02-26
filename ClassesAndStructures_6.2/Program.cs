13925be6afbd1b4ca76a21441bf9eca7c46400ef
Ипполитов Алексей <setgecko@gmail.com>
2023-02-26 16:18:11 +0300

    Пример создания классов и структур

--- ClassesAndStructures_6.2/Program.cs
+++ ClassesAndStructures_6.2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+
         }
+        class Human
+        {
+            public string name;
+            public int age;
+
+            public void Greetings()
+            {
+                Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+            }
+        }
+
+        struct Animal
+        {
+            public string type;
+            public string name;
+            public int age;
+
+            public void Info()
+            {
+                Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+            }
+        }
+
     }
 }
