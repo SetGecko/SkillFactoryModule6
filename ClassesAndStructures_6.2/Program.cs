d556832483a64f6c3a5c8232c75396643cfc632a
Ипполитов Алексей <setgecko@gmail.com>
2023-02-26 16:44:23 +0300

    Практика 6.2 - Добавление конструкторов в класс Human

--- ClassesAndStructures_6.2/Program.cs
+++ ClassesAndStructures_6.2/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Human human= new Human();
+            Human human = new Human();
             human.Greetings();
+            Console.WriteLine("---");
 
-            human.name = "Алексей";
-            human.age = 35;
+            human = new Human("Алексей");
+            human.Greetings();
+            Console.WriteLine("---");
+
+            human = new Human("Жужа", 40);
             human.Greetings();
         }
         class Human
@@ -19,6 +23,24 @@
             public void Greetings()
             {
                 Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+            }
+
+            public Human()
+            {
+                name = "Неизвестно";
+                age = 35;
+            }
+
+            public Human(string n)
+            {
+                name = n;
+                age = 35;
+            }
+
+            public Human(string n, int a)
+            {
+                name = n;
+                age = a;
             }
         }
 
