eb3be1727e236d2494954b9b3ef1871d0d7ceb89
Ипполитов Алексей <setgecko@gmail.com>
2023-02-26 16:48:36 +0300

    Выполнил задание 6.2

--- ClassesAndStructures_6.2/Program.cs
+++ ClassesAndStructures_6.2/Program.cs
@@ -56,5 +56,23 @@
             }
         }
 
+        class Pen
+        {
+            public string color;
+            public int cost;
+
+            public Pen()
+            {
+                color = "Черный";
+                cost = 100;
+            }
+
+            public Pen(string penColor, int penCost)
+            {
+                color = penColor;
+                cost = penCost;
+            }
+        }
+
     }
 }
