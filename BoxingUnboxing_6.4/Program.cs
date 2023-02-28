597e61acfabc3f774a79aeab4e9e2d0573a41ca0
Ипполитов Алексей <setgecko@gmail.com>
2023-02-28 19:31:23 +0300

    Практика упаковка / распаковка

---
+++ BoxingUnboxing_6.4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BoxingUnboxing_6._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Процесс упаковки( неявный):
+            int i = 123;
+            // Упаковка значения i в объект o
+            object o = i;
+
+            // или явная упаковка object o = (object)i;
+
+            //Распаковка же происходит только явным образом
+
+            int j = (int)o;
+
+
+
+        }
+    }
+}
