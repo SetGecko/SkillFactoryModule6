b6bd6bc2882a12c792ca9130ec452d85a6d0efed
Ипполитов Алексей <setgecko@gmail.com>
2023-02-28 19:02:43 +0300

    Выполнил задание 6.3.1

---
+++ Task_6.3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_6._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static Department GetCurrentDepartment()
+        {
+            if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            {
+                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+            }
+        }
+    }
+    class Company
+    {
+        public string Type;
+        public string Name;
+    }
+
+    class Department
+    {
+        public Company Company;
+        public City City;
+    }
+
+    class City
+    {
+        public string Name;
+    }
+}
