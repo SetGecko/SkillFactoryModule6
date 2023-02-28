97b30fdfa737ad06e960657af7223a6781dd9a45
Ипполитов Алексей <setgecko@gmail.com>
2023-02-28 19:04:50 +0300

    Выполнил задание 6.3.2

---
+++ Task_6.3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_6._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+        }
+    }
+}
