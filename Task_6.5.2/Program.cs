8bbbf370ba08ed5599a09de51a7f38effdca2c1f
Ипполитов Алексей <setgecko@gmail.com>
2023-03-01 21:58:51 +0300

    Выполнил задание 6.5.2

---
+++ Task_6.5.2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_6._5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Circle
+    {
+        public double radius;
+        public double Square()
+        {
+        }
+        public double Length()
+        {
+        }
+
+    }
+    class Triangle
+    {
+        public int a;
+        public int b;
+        public int c;
+
+        public double Square()
+        {
+        }
+
+        public double Perimeter()
+        {
+        }
+    }
+
+    class Square
+    {
+        public int side;
+
+        public double Square()
+        {
+        }
+
+        public double Perimeter()
+        {
+        }
+    }
+}
