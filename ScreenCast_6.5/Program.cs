bbed1f8f2bc61d007777388bf39a4a19efe92067
Ипполитов Алексей <setgecko@gmail.com>
2023-03-01 21:30:46 +0300

    Практика по скринкасту 6.5

---
+++ ScreenCast_6.5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ScreenCast_6._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Car
+    {
+        public double Fuel;
+
+        public int Mileage;
+
+        public Car()
+        {
+            Fuel = 50;
+            Mileage = 0;
+        }
+        public void Move()
+        {
+            // Движениае на 1 километр
+            Mileage++;
+            Fuel -= 0.5;
+        }
+        public void FillTheCar()
+        {
+            Fuel = 50;
+        }
+    }
+}
