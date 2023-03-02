10258caf552f4c5ef1c7c78389d7e456d2f4963c
Ипполитов Алексей <setgecko@gmail.com>
2023-03-02 13:38:58 +0300

    Модификаторы доступа свойств

---
+++ AccessModifiersGetSet_6.6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AccessModifiersGetSet_6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class User
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+            }
+        }
+    }
+}
