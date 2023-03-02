6dbdc4c7a053fee929c2a223e8a9e3e1e5c01ce8
Ипполитов Алексей <setgecko@gmail.com>
2023-03-02 10:27:25 +0300

    Пример применения геттеров и сеттеров

---
+++ Properties_6.6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Properties_6._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.Age = 35;
+
+            Console.WriteLine(user.Age);
+
+            Console.ReadKey();
+        }
+    }
+    class User
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
