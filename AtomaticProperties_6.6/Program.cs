c3c1501e2424decd0bb016584befd9f380509c4f
Ипполитов Алексей <setgecko@gmail.com>
2023-03-02 13:42:17 +0300

    Автоматические свойства

---
+++ AtomaticProperties_6.6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AtomaticProperties_6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class User
+    {
+        public string Login { get; set; }
+        public int Age { get; set; }
+    }
+}
