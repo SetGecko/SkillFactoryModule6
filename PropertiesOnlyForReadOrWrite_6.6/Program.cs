2f4fc6ee7be0958a4a6d9c9e59dbfc552e55d123
Ипполитов Алексей <setgecko@gmail.com>
2023-03-02 13:34:02 +0300

    Свойства только для чтения и только для записи

---
+++ PropertiesOnlyForReadOrWrite_6.6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PropertiesOnlyForReadOrWrite_6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class User
+    {
+        private string login;
+        private DateTime lastSeen;
+        // Только для чтения
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+        }
+        // Только для записи
+        public DateTime LastSeen
+        {
+            set
+            {
+                lastSeen = value;
+            }
+        }
+    }
+}
