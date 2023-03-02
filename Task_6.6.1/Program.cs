07dac3e721626f84b46dea261ff03b30f56cde5d
Ипполитов Алексей <setgecko@gmail.com>
2023-03-02 10:20:03 +0300

    Выполнил задание 6.6.1

---
+++ Task_6.6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_6._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class TrafficLight
+    {
+        private void ChangeColor(string color)
+        {
+
+        }
+        public string GetColor()
+        {
+
+        }
+    }
+}
